ff196475d88edc86f17ff5fd9f4e3dac0f51fdd3
Marius Jonassen <mariusojonassen@gmail.com>
2022-02-16 09:56:04 +0100

    updated PLOT__Radiometers.m script with better xticks and limits for the start and end times

--- 02_Matlab/Matlab_PLOT_scripts/PLOT__Radiometers.m
+++ 02_Matlab/Matlab_PLOT_scripts/PLOT__Radiometers.m
@@ -3,8 +3,12 @@
 %   READIN__Radiometers
 %% INPUT: Set the CB ID (qq) to choose which radiometer you want to plot data from
 
-qq = 3; %
+qq = 2; %
 
+
+id1 = datenum([2022 02 06 00 00 00]);
+id2 = datenum([2022 02 14 00 00 00]);
+idd = find(RAD(qq).time >= id1 & RAD(qq).time <= id2);
 
 clc
 
@@ -12,47 +16,47 @@ close all
 
 subplot(2,3,1)
 hold on
-plot(RAD(qq).time,RAD(qq).sw_down,'-','LineWidth',2)
-xlabel('Time (dd.mm)')
+plot(RAD(qq).time(idd),RAD(qq).sw_down(idd),'-','LineWidth',2)
+xlabel('Time (dd)')
 ylabel('Shortwave down (W m^{-2})')
 ylim([0 360])
-datetick('x')
+datetick('x','dd','keeplimits')
 grid on
 
 subplot(2,3,2)
 hold on
-plot(RAD(qq).time,RAD(qq).sw_up,'-','LineWidth',2)
-xlabel('Time (dd.mm)')
+plot(RAD(qq).time(idd),RAD(qq).sw_up(idd),'-','LineWidth',2)
+xlabel('Time (dd)')
 ylabel('Shortwave up (W m^{-2})')
 ylim([0 360])
-datetick('x')
+datetick('x','dd','keeplimits')
 grid on
 
 subplot(2,3,3)
 hold on
-plot(RAD(qq).time,RAD(qq).lw_down,'-','LineWidth',2)
-xlabel('Time (dd.mm)')
+plot(RAD(qq).time(idd),RAD(qq).lw_down(idd),'-','LineWidth',2)
+xlabel('Time (dd)')
 ylabel('Longwave down (W m^{-2})')
 ylim([100 450])
-datetick('x')
+datetick('x','dd','keeplimits')
 grid on
 
 subplot(2,3,4)
 hold on
-plot(RAD(qq).time,RAD(qq).lw_up,'-','LineWidth',2)
-xlabel('Time (dd.mm)')
+plot(RAD(qq).time(idd),RAD(qq).lw_up(idd),'-','LineWidth',2)
+xlabel('Time (dd)')
 ylabel('Longwave up (W m^{-2})')
 ylim([100 450])
-datetick('x')
+datetick('x','dd','keeplimits')
 grid on
 
 subplot(2,3,5)
 hold on
-plot(RAD(qq).time,RAD(qq).Tsurf,'-','LineWidth',2)
-xlabel('Time (dd.mm)')
+plot(RAD(qq).time(idd),RAD(qq).Tsurf(idd),'-','LineWidth',2)
+xlabel('Time (dd)')
 ylabel('Surface temperature (\circC)')
-ylim([-20 15])
-datetick('x')
+ylim([-30 0])
+datetick('x','dd','keeplimits')
 grid on
 
 
